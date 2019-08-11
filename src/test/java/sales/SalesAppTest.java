afdcbf6a1ab07308360c28f0cbc911f71be3866d
linguangxiong <854501715@qq.com>
2019-08-12 01:49:30 +0800

    Test(SalesApp):test for method getSalesBySalesId

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -1,8 +1,24 @@
 package sales;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class SalesAppTest {
+
+
+	@Mock
+	SalesDao salesDao;
+
+	@InjectMocks
+	SalesApp salesApp = new SalesApp();
 
 	@Test
 	public void testGenerateReport() {
@@ -11,5 +27,26 @@ public class SalesAppTest {
 		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
 		
 	}
+
+
+	@Test
+	public void shoule_return_Sales_when_call_getSalesBySalesId_given_SalesId(){
+
+		String salesId = "Test";
+		Sales sales1 = new Sales();
+		sales1.setSupervisor(true);
+		when(salesDao.getSalesBySalesId(salesId)).thenReturn(sales1);
+
+		Sales sales = salesApp.getSalesBySalesId(salesId);
+
+		assertTrue(sales.isSupervisor());
+
+	}
+
+
+
+
+
+
 
 }
